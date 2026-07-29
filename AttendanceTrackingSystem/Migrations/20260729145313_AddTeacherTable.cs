9ad77af127ef93dd9aa39df2ff316194a5b40d68
chuanyang22 <teechuanyang0322@gmail.com>
2026-07-29 22:58:24 +0800

    added

---
+++ AttendanceTrackingSystem/Migrations/20260729145313_AddTeacherTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AttendanceTrackingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeacherTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
